07b94372c1986eeeacb248631d7a7ac6c9ae3a93
natalieconan <natalieconan17@gmail.com>
2024-05-10 01:09:54 +0700

    fix: update image url to relative url

--- src/pages/welcome/PopularItems.tsx
+++ src/pages/welcome/PopularItems.tsx
@@ -6,7 +6,7 @@ export const PopularItems = () => {
         <div className="week-hot-sale-container">
             <h1>
                 <img id="flashsale-icon" 
-                    src="https://github.com/enwcoin/ecoinfe/blob/78b636dc0994abfe466d11293c1c9b4920233b89/src/assets/flashsale_icon.png"
+                    src="src/assets/flashsale_icon.png"
                     alt="flashsale_icon" 
                     sizes="small" 
                 />
