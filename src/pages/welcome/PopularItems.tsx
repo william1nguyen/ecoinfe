bb4ebedf8496fa2aa8fc42f55ebae50a3593ac59
natalieconan <natalieconan17@gmail.com>
2024-05-10 00:47:52 +0700

    fix: fix image url

--- src/pages/welcome/PopularItems.tsx
+++ src/pages/welcome/PopularItems.tsx
@@ -5,7 +5,7 @@ export const PopularItems = () => {
     return (
         <div className="week-hot-sale-container">
             <h1>
-                <img id="flashsale-icon" src="./src/assets/flashsale_icon.png" alt="flashsale_icon" sizes="small" />
+                <img id="flashsale-icon" src="src/assets/flashsale_icon.png" alt="flashsale_icon" sizes="small" />
                 WEEKEND HOT 
                 <span> SALE</span>
             </h1>
